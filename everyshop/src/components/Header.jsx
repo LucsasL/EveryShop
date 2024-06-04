1ca3030c7bce50a07ce025e3f4bcb8f7c4319c58
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-06-03 22:54:32 -0300

    Main Header Settings done

--- everyshop/src/components/Header.jsx
+++ everyshop/src/components/Header.jsx
@@ -1,31 +1,78 @@
+// Importing Hooks
+import { useState } from "react";
+
 import { FiHeart } from "react-icons/fi";
 import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const lateralNav = () => {
+    setMenuOpen(!menuOpen);
+  }
+
   return (
     <>
       <header>
         <div>
-          <input
-            type="text"
-            className="searchInput"
-            placeholder="Enter your search shoes"
-            onChange={(e) => (e)}
-          />
+          <div className="logo">
+            <button onClick={() => lateralNav()}>
+              menu
+            </button>
 
-          <a href="#">
-            <FiHeart className="nav-icons" />
-          </a>
+            <h1>
+              EveryShop
+            </h1>
+          </div>
+          
+          <div className="inpDiv">
+            <input
+              type="text"
+              className="searchInput"
+              placeholder="Enter your search shoes"
+              onChange={(e) => (e)}
+            />
+            <a href="http://">
+              <FiHeart className="nav-icons" />
+            </a>
+            <a href="https://">
+              <AiOutlineShoppingCart className="nav-icons" />
+            </a>
+            <a href="https://">
+              <AiOutlineUserAdd className="nav-icons" />
+            </a>
+          </div>
 
-          <a href="#">
-            <AiOutlineShoppingCart className="nav-icons" />
-          </a>
+          <div className="configDiv">
+            <label htmlFor="themeInp">
+              <input type="checkbox" name="theme" id="themeInp" />
+            </label>
 
-          <a href="#">
-            <AiOutlineUserAdd className="nav-icons" />
-          </a>
+            <select name="languageSel" id="langSel">
+              <option value="English">English</option>
+              <option value="Português">Português</option>
+            </select>
+          </div>
         </div>
       </header>
+
+      {
+        menuOpen ? (
+          <nav>
+            <div>
+              <ul>
+                <li><a href="https://">Shoes</a></li>
+                <li><a href="https://">Shoes</a></li>
+                <li><a href="https://">Shoes</a></li>
+                <li><a href="https://">Shoes</a></li>
+                <li><a href="https://">Shoes</a></li>
+              </ul>
+            </div>
+          </nav>
+        ) : (
+        <></>
+      )
+      }
     </>
   );
 }
