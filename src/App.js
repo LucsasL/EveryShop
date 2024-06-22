48ccf848d7e08049b451c2488975ba064dfdd97f
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-06-22 12:29:41 -0300

    Small footer update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // Hook Imports
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Components Imports
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,52 @@ const Footer = () => {
   return (
     <>
       <footer>
-        <p>
-          Built by <a href="http://github.com/lucsasl" target="_blank" rel="noopener noreferrer">Lucsas</a>
-        </p>
+        <div className="footerNav">
+          <h1>
+            Everyshop
+          </h1>
+
+          <div className="links">
+            <div>
+              <h2>
+                Our Policy
+              </h2>
+              <ul>
+                <li>
+                  Test
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <h2>
+                Quick Links
+              </h2>
+              <ul>
+                <li>
+                  Test
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="credit">
+          <p>This is a fictitious company</p>
+          <p>
+            Built by{" "}
+            <a
+              href="https://github.com/lucsasl"
+              target="blank"
+              rel="noopener noreferrer"
+            >
+              Lucsas
+            </a>
+          </p>
+        </div>
       </footer>
     </>
-  )
+  );
 }
 
 export default Footer
